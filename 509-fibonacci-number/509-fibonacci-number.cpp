85b6cf974383a4b50edcc340b4dac5b8003eafe7
Prasanth Kumar <75605140+prasanthkumarseepana@users.noreply.github.com>
2022-07-07 13:24:17 +0530

    Time: 4 ms (52.46%), Space: 5.9 MB (80.07%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<=1)return n;
-        else return fib(n-1)+fib(n-2);// Simple recursion based
+    int f[n + 2];//Using dynamic programming
+    f[0] = 0;
+    f[1] = 1;
+ 
+    for(int i = 2; i <= n; i++)
+    {
+        
+       f[i] = f[i - 1] + f[i - 2];//Add the previous 2 number in the series and store it
+    }
+    return f[n];
     }
 };
